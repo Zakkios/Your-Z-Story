38ae3c0f9c67eacd39ae8ae8440765d22fb66cb6
Zakkios <laskari.zakaria@gmail.com>
2026-03-23 11:24:22 +0100

    feat(client): remove default React starter content and add first choice screen and connect to backend API

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FirstChoice from './components/FirstChoice';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstChoice />
+    </div>
+  );
+}
+
+export default App;
