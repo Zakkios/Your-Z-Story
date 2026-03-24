6fe0e52dfaec8a67623bfa0eb0953cd701472106
Zakkios <laskari.zakaria@gmail.com>
2026-03-24 15:56:01 +0100

    feat(component): ajouter le composant Intro avec gestion de chapitre et intégration dans GamePage

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import FirstChoice from '../components/FirstChoice';
+import { useState } from 'react';
+import BulmaCar from '../components/BulmaCar';
+import Intro from '../components/Intro';
+import { sendChoice } from '../services/api';
 
 const GamePage = () => {
+    const [gameStarted, setGameStarted] = useState(false);
+
+    const startGame = () => {
+        setGameStarted(true);
+        try {
+            sendChoice("1");
+        } catch (error) {
+            console.error('Error starting game:', error);
+            alert('Une erreur est survenue lors du démarrage du jeu. Veuillez réessayer.');
+        }
+    };
+
     return (
-        <div className="p-4">
-            <FirstChoice />
+        <div>
+            {gameStarted ? null : <Intro startGame={startGame} />}
+            {gameStarted && <BulmaCar />}
         </div>
     );
 };
