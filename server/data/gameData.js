87e54ca5ef01916e39979fe89ec43d110a63d2d0
Zakkios <laskari.zakaria@gmail.com>
2026-03-25 12:33:42 +0100

    fix(gameData): corriger la structure des chapitres en utilisant les identifiants

--- server/data/gameData.js
+++ server/data/gameData.js
@@ -3,8 +3,8 @@ import chapter1 from "./chapters/chapter1.js"
 
 const gameData = {
     chapters: {
-        ...chapter0,
-        ...chapter1
+        [chapter0.id]: chapter0,
+        [chapter1.id]: chapter1
     }
 }
 
