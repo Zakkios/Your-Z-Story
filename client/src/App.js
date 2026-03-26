8984cd226fab0e070ef153d1839d8128805c51a3
Zakkios <laskari.zakaria@gmail.com>
2026-03-26 16:23:13 +0100

    fix: ajouter min-h-screen pour garantir un affichage correct de la page de jeu

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import GamePage from './pages/GamePage';
 
 function App() {
   return (
-    <div className="container mx-auto text-center h-screen flex items-center justify-center">
+    <div className="container min-h-screen mx-auto text-center h-screen flex items-center justify-center">
       <GamePage />
     </div>
   );
