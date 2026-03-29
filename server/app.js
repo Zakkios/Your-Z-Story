2060c720fed313c327d98c21d5a2a4e2e22fd0eb
Zakkios <laskari.zakaria@gmail.com>
2026-03-29 13:59:05 +0200

    refacto: ajout de la gestion des erreurs et configuration du port du serveur

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import choiceRoutes from './routes/choiceRoutes.js';
+import errorHandler from './middleware/errorHandler.js';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/', choiceRoutes);
+
+app.use(errorHandler);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import app from './app.js';
+import config from './config/index.js';
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
 });
