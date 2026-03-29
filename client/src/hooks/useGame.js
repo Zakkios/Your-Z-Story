6cfc62b66db4184f044d2d501e83ee2c8b83f407
Zakkios <laskari.zakaria@gmail.com>
2026-03-29 14:01:22 +0200

    refacto: réorganisation des fichiers et ajout de la logique de jeu avec gestion des erreurs

---
+++ client/src/hooks/useGame.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { fetchScene, sendChoice } from '../services/api';
+
+const useGame = () => {
+    const [gameState, setGameState] = useState(null);
+    const [isGameOver, setIsGameOver] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const load = async () => {
+            try {
+                const data = await fetchScene("chapter0", "intro");
+                if (!data || data.status !== 200) {
+                    setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
+                    return;
+                }
+                const scene = data.scene;
+                setGameState(scene);
+                setIsGameOver(scene?.status === "gameover");
+            } catch (err) {
+                console.error("Error fetching initial scene:", err);
+                setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
+            }
+        };
+
+        load();
+    }, []);
+
+    const handleChoice = async (choiceId) => {
+        const choice = gameState.choices.find((choice) => choice.id === choiceId);
+        if (!choice) {
+            setError("Une erreur s'est produite lors du traitement de votre choix. Veuillez réessayer plus tard.");
+            return;
+        }
+
+        const payload = {
+            chapterId: choice.nextChapterId,
+            sceneId: choice.nextSceneId,
+            choiceId
+        };
+        setGameState(null);
+        try {
+            const data = await sendChoice(payload);
+            if (!data || data.status !== 200) {
+                setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
+                return;
+            }
+            const scene = data.scene;
+            setGameState(scene);
+            setIsGameOver(scene?.status === "gameover");
+        } catch (err) {
+            console.error("Error sending choice:", err);
+            setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
+        }
+    };
+
+    return { gameState, isGameOver, error, handleChoice };
+};
+
+export default useGame;
--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -1,65 +1,13 @@
-import { useEffect, useState } from 'react';
-import { fetchScene, sendChoice } from '../services/api';
-import SceneRenderer from '../components/SceneRenderer';
-import GameOverSceneRenderer from '../components/GameOverSceneRenderer';
+import useGame from '../hooks/useGame';
+import SceneRenderer from '../components/game/SceneRenderer';
+import GameOverSceneRenderer from '../components/game/GameOverSceneRenderer';
 
 const GamePage = () => {
-    const [gameState, setGameState] = useState(null);
-    const [isGameOver, setIsGameOver] = useState(false);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const load = async () => {
-            try {
-                const data = await fetchScene("chapter0", "intro");
-                if (!data || data.status !== 200) {
-                    setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
-                    return;
-                }
-                const scene = data.scene;
-                setGameState(scene);
-                setIsGameOver(scene?.status === "gameover");
-            } catch (err) {
-                console.error("Error fetching initial scene:", err);
-                setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
-            }
-        };
-
-        load();
-    }, []);
-
-    const handleChoice = async (choiceId) => {
-        const choice = gameState.choices.find((choice) => choice.id === choiceId);
-        if (!choice) {
-            setError("Une erreur s'est produite lors du traitement de votre choix. Veuillez réessayer plus tard.");
-            return;
-        }
-
-        const payload = {
-            chapterId: choice.nextChapterId,
-            sceneId: choice.nextSceneId,
-            choiceId
-        };
-        setGameState(null)
-        try {
-            const data = await sendChoice(payload);
-            if (!data || data.status !== 200) {
-                setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
-                return;
-            }
-            const scene = data.scene;
-            setGameState(scene);
-            setIsGameOver(scene?.status === "gameover");
-        } catch (err) {
-            console.error("Error sending choice:", err);
-            setError("Erreur lors du chargement de la scène. Veuillez réessayer plus tard.");
-        }
-    };
+    const { gameState, isGameOver, error, handleChoice } = useGame();
 
     if (error) {
         return <div className="text-red-500">{error}</div>;
     }
-
 
     if (!gameState) {
         return <div>Chargement...</div>;
@@ -83,4 +31,3 @@ const GamePage = () => {
 };
 
 export default GamePage;
-
