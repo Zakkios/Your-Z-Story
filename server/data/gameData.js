31fb73b67aa4237bcab019531881f47ec5f8a841
Zakkios <laskari.zakaria@gmail.com>
2026-04-01 09:59:58 +0200

    feat: suppression de chapter0 du jeu et mise à jour des données de chapitre

--- server/data/gameData.js
+++ server/data/gameData.js
@@ -1,9 +1,7 @@
-import chapter0 from "./chapters/chapter0.js"
 import chapter1 from "./chapters/chapter1.js"
 
 const gameData = {
     chapters: {
-        [chapter0.id]: chapter0,
         [chapter1.id]: chapter1
     }
 }
