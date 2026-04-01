7736cf24c5a8d22dc5f35c57b08924ef138054af
Zakkios <laskari.zakaria@gmail.com>
2026-04-01 09:59:35 +0200

    feat: mise à jour de la structure de routage avec ajout de HomePage et ChapterPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './styles/App.css';
-import GamePage from './pages/GamePage';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChapterPage from './pages/ChapterPage';
 
 function App() {
   return (
     <div className="container min-h-screen mx-auto text-center h-screen flex items-center justify-center">
-      <GamePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chapter/:chapterId" element={<ChapterPage />} />
+      </Routes>
     </div>
   );
 }
