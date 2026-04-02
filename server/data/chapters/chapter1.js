18f273a3a16b4440737150e563aeca68fab7c548
Zakkios <laskari.zakaria@gmail.com>
2026-04-02 17:23:35 +0200

    feat: amélioration du formatage du texte dans chapter1.js

--- server/data/chapters/chapter1.js
+++ server/data/chapters/chapter1.js
@@ -9,7 +9,7 @@ const chapter1 = {
         bulmaCar: {
             id: "bulmaCar",
             title: "C’est quoi ce monstre ?!",
-            text: "<em>Sur le retour de la pêche, Goku manque de peu de se faire percuté par une voiture.</em> <br> <strong>Goku :</strong> “C’est quoi ce monstre ?! Il en veut à mon poisson ?!”",
+            text: `<span class="cc-wild-words-italic">Sur le retour de la peche, Goku manque de peu de se faire percute par une voiture.</span> <br> <span class="cc-wild-words-bold-italic">Goku :</span> “C’est quoi ce monstre ?! Il en veut a mon poisson ?!”`,
             status: "ongoing",
             shots: [
                 {
@@ -63,7 +63,7 @@ const chapter1 = {
         carFight: {
             id: "carFight",
             title: "Un choix important",
-            text: "Bulma te demande de lui donner la Dragon Ball de ton grand-père pour qu'elle puisse exaucer son vœu. Que fais-tu ?",
+            text: "Bulma te demande de lui donner la Dragon Ball de ton grand-pere pour qu'elle puisse exaucer son voeu. Que fais-tu ?",
             status: "ongoing",
             shots: [
                 {
@@ -117,7 +117,7 @@ const chapter1 = {
                 {
                     id: "shot7",
                     image: "",
-                    text: `<em>Goku rencontre Bulma pour la première fois.</em> Depuis la mort de son grand-père, il a toujours vécu seul… <strong>c’est même la première humaine qu’il rencontre.</strong> <br/><em>Leur échange est… plutôt étrange.</em> Sans trop se poser de questions, Goku décide de l’emmener chez lui. <br/>À l’intérieur, un objet d’un <strong>orange brillant</strong> attire immédiatement l’attention de <strong>Bulma</strong>.`,
+                    text: `<span class="cc-wild-words-italic">Goku rencontre Bulma pour la premiere fois.</span> Depuis la mort de son grand-pere, il a toujours vecu seul… <span class="cc-wild-words-bold-italic">c’est meme la premiere humaine qu’il rencontre.</span> <br/><span class="cc-wild-words-italic">Leur echange est… plutot etrange.</span> Sans trop se poser de questions, Goku decide de l’emmener chez lui. <br/>À l’interieur, un objet d’un <span class="cc-wild-words-bold-italic">orange brillant</span> attire immediatement l’attention de <span class="cc-wild-words-bold-italic">Bulma</span>.`,
                     animation: "",
                     transition: "",
                     isImageType: "false"
@@ -133,7 +133,7 @@ const chapter1 = {
                 {
                     id: "shot9",
                     image: "/images/chapter1/car-fight/9.png",
-                    text: `Elle lui apprend alors l'existence des <strong>Dragon Balls</strong> et de leur pouvoir : <em>“Quand on rassemble les sept <strong>Dragon Balls</strong>, le dragon <strong>Shenron</strong> apparaît pour exaucer n'importe lequel de nos vœux !!”</em>.`,
+                    text: `Elle lui apprend alors l'existence des <span class="cc-wild-words-bold-italic">Dragon Balls</span> et de leur pouvoir : <span class="cc-wild-words-italic">“Quand on rassemble les sept <span class="cc-wild-words-bold-italic">Dragon Balls</span>, le dragon <span class="cc-wild-words-bold-italic">Shenron</span> apparait pour exaucer n'importe lequel de nos voeux !!”</span>.`,
                     animation: "",
                     transition: "",
                     isImageType: "true"
@@ -165,7 +165,7 @@ const chapter1 = {
         carRun: {
             id: "carRun",
             title: "Tu as choisi de prendre la fuite !",
-            text: "Tu prends la fuite et te cache dans la montagne. Mais en rentrant chez toi, tu ne retrouve pas la dragon ball de grand-père... Impossible de retrouver cette voleuse...",
+            text: "Tu prends la fuite et te cache dans la montagne. Mais en rentrant chez toi, tu ne retrouve pas la dragon ball de grand-pere... Impossible de retrouver cette voleuse...",
             status: "gameover",
             shots: [
                 {
