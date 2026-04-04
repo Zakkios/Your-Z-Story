c5510accfd8cfd0b76613646268665ae0e22c1c6
Zakkios <laskari.zakaria@gmail.com>
2026-04-04 17:35:40 +0200

    feat: add env variable REACT_APP_API_URL for API configuration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
 export const fetchScene = async (chapterId, sceneId) => {
     const response = await fetch(`${API_URL}/chapter/${chapterId}/scene/${sceneId}`, {
