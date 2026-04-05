d3e2e0f6cdefb78e679b1627af89217fbc7fd74c
Zakkios <laskari.zakaria@gmail.com>
2026-04-05 14:00:46 +0200

    feat: add loading component

---
+++ client/src/components/ui/Loading.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+        <div>
+            <img src="/images/loading.png" alt="Chargement" className="animate-spin max-h-[20vh]" />
+            <p className="pt-4">Chargement...</p>
+        </div>
+    );
+}
+
+export default Loading;
